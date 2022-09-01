3c367d8872d640cab1ae3038788bc2feb4928af8
Dominic Prior <dominicprior42@gmail.com>
2022-09-01 08:02:30 +0100

    Tidy 08.textures1.twgl.js

--- 08.textures1.twgl.js
+++ 08.textures1.twgl.js
@@ -25,7 +25,7 @@ gl.useProgram(programInfo.program);
 const vertexBufferData = new Float32Array([ -.9,.9,  -.9,-.9,  .9,.9,  .9,-.9, ]);
 const texCoordBufferData = new Float32Array([ 0,1, 0,0, 1,1, 1,0, ]);
 
-const pixels = new Uint8Array([  //-------
+const pixels = new Uint8Array([
 	255,255,255,		230,25,75,			60,180,75,			255,225,25,
 	67,99,216,			245,130,49,			145,30,180,			70,240,240,
 	240,50,230,			188,246,12,			250,190,190,		0,128,128,
@@ -37,28 +37,8 @@ let arrays = {
     aTexCoord: { numComponents: 2, data: texCoordBufferData, },
 }
 let bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
-
 twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
-/*
-const pixelBuffer = gl.createBuffer();   //-------?????
-gl.bindBuffer(gl.PIXEL_UNPACK_BUFFER, pixelBuffer);   //-------?????
-gl.bufferData(gl.PIXEL_UNPACK_BUFFER, pixels, gl.STATIC_DRAW);   //-------?????
-*/
-/* ========= this gets replaced
-const checkerTex = gl.createTexture();
-gl.bindTexture(gl.TEXTURE_2D, tex);
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 2, 2, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([
-    255,255,255,255,
-    192,192,192,255,
-    192,192,192,255,
-    255,255,255,255,
-  ]));
-gl.generateMipmaps(gl.TEXTURE_2D);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-============ */
 
-// /* +++++++++++ new stuff we want
 const texture = twgl.createTexture(gl, {
     mag: gl.NEAREST,
     min: gl.LINEAR,
@@ -66,36 +46,8 @@ const texture = twgl.createTexture(gl, {
 	width: 4,
 	format: gl.RGB
 });
-// +++++++++++ */
 
+const uniforms = { uSampler: texture }
+twgl.setUniforms(programInfo, uniforms)
 
-//const textureSlot = 1;   //--------
-//gl.activeTexture(gl.TEXTURE0 + textureSlot);   //---------
-//gl.uniform1i(gl.getUniformLocation(programInfo.program, 'uSampler'), textureSlot);  //--------
-
-// const texture = gl.createTexture();     /////////---------=========
-
-const uniforms = { uSampler: texture }   //++++++++
-
-
-twgl.setUniforms(programInfo, uniforms);  //+++++++++++
-/*
-gl.bindTexture(gl.TEXTURE_2D, texture);  /////////--------========
-
-gl.texImage2D(gl.TEXTURE_2D,  /////////------========
-	          0,  // mipmap level
-			  gl.RGB, // format used by the fragment shader
-			  4, 4,   // width and height of the source image
-			  0,      // always zero
-			  gl.RGB, // source format
-			  gl.UNSIGNED_BYTE,
-			  0);     // offset in bytes into the texture pixel array
-
-
-gl.generateMipmap(gl.TEXTURE_2D);   /////////--------========
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST); //////---===
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
-*/
 twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_STRIP)
